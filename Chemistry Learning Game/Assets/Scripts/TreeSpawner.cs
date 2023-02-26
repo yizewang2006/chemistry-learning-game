ed3f34c95cea0dce4130c49c691d798fbfaaf34b
Moddwyn <nathaliaandaya@gmail.com>
2023-02-26 14:58:46 -0800

    Tree Spawner Update

--- Chemistry Learning Game/Assets/Scripts/TreeSpawner.cs
+++ Chemistry Learning Game/Assets/Scripts/TreeSpawner.cs
@@ -4,34 +4,48 @@ using UnityEngine;
 
 public class TreeSpawner : MonoBehaviour
 {
-    public GameObject treePrefab;
+    public Vector2 spawnSize;
+    public float spawnHeightMin;
+    public Vector2 spacingRangeDense;
+    public Vector2 spacingRangeDefault;
+    public Vector2 minDenseSize;
+    public Vector2 maxDenseSize;
+    public GameObject[] treePrefab;
+    public LayerMask terrainLayer;
+
+    RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
+        SpawnDefaultDense(new Vector2(0,0), spawnSize, spacingRangeDefault);
+    }
+
+    void SpawnDefaultDense(Vector2 startingPos, Vector2 endingPos, Vector2 spacing)
+    {
+        float randSpace = 1;
+        for (float row = startingPos.x; row < endingPos.x; row+=randSpace)
+        {
+            for (float col = startingPos.y; col < endingPos.y; col+=randSpace)
+            {
+                randSpace = Random.Range(spacing.x, spacing.y);
+                if(Physics.Raycast(new Vector3(row, 100, col), Vector3.down, out hit, Mathf.Infinity, terrainLayer))
+                {
+                    if(hit.point.y >= spawnHeightMin)
+                    {
+                        if(Random.Range(0,2) == 0) continue;
+                        SpawnTree(hit.point, Quaternion.Euler(hit.normal));
+                    }
+                }
+                
+            }
+            randSpace = Random.Range(spacingRangeDefault.x, spacingRangeDefault.y);
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        ShootRaycast();
-    }
-    void ShootRaycast()
-    {
-        Vector3 direction;
-
-        Ray ray = new Ray(transform.position, Vector3.down);
-        Debug.DrawLine(transform.position, Vector3.down, color: Color.red);
-
-        RaycastHit targetPoint;
-        Physics.Raycast(ray, out targetPoint);
-
-        Debug.Log(targetPoint.point);
     }
 
     void SpawnTree(Vector3 location, Quaternion rotation)
     {
-        Instantiate(treePrefab, position: location, rotation: rotation);
+        Instantiate(treePrefab[Random.Range(0, treePrefab.Length)], location, rotation);
     }
 }
